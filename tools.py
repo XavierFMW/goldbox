ed030064633c6d58ed157831b7444a761cf79b5e
Xavier M.W <69274109+XavierFMW@users.noreply.github.com>
2022-09-17 06:30:29 -0600

    Imported timeit
    Imported the "timeit" function from an external source.

--- tools.py
+++ tools.py
@@ -4,3 +4,17 @@ def file_to_list(filename):
     with open(filename, "r") as f:
         lines = [line.strip() for line in f.readlines() if line != ""]
         return lines
+
+
+def timeit(function):
+
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        response = function(*args, **kwargs)
+        end = time.time()
+
+        difference = end - start
+        print(f"{function.__name__}: {round(difference * 1000)}")
+        return response
+
+    return wrapper
