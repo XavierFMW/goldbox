e62f551c41330d4e309b42938a880cc11a221c1f
Xavier M.W <69274109+XavierFMW@users.noreply.github.com>
2022-09-18 16:53:19 -0600

    Added RNG methods
    Added functions which allow the generation of random numbers.

--- tools.py
+++ tools.py
@@ -1,3 +1,5 @@
+import random
+import secrets
 
 def file_to_list(filename):
 
@@ -18,4 +20,21 @@ def timeit(function):
         return response
 
     return wrapper
+
+
+def get_random_int(start, end, inclusive=False, truly_random=False):
+
+	if inclusive:
+		end += 1
+
+	if truly_random:
+		return secrets.randbelow(end - start) + start
+
+	else:
+		return random.randrange(start, end)
+
+
+def get_random_float(start, end, inclusive=False, truly_random=False):
+	base = get_random_int(start*100, end*100, inclusive, truly_random)
+	return base / 100	
 
